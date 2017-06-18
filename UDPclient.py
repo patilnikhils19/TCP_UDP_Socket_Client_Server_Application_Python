cdc32745edc6163d06b1cbe289737ddc34af55c7
Nikhil Patil <patilnikhils19@gmail.com>
2017-06-18 11:52:35 -0600

    UDP Based Client Server

---
+++ UDPclient.py
@@ -0,0 +1,67 @@
+import os
+import argparse
+import sys
+import socket
+
+
+def client():
+
+    host = args.serverIP #Get Host IP from arguments
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#create socket
+    sock.bind((host, args.port))
+    server = (host,args.port)
+    command = str(args.command)# Take command from argument
+    print command
+    if not args.delay:
+       args.delay = 0
+       delay = str(args.delay)
+    else :
+       delay = str(args.delay)
+    if not args.number:
+       args.number = 1
+       number =str(args.number)
+    else :
+       number =str(args.number)
+
+    sock.sendto(command, server)#Send Command to server
+    sock.sendto(delay, server)#Send Delay to server
+    sock.sendto(number, server)#send Execution count to server
+    number = int(number)
+    while(number):
+         data, address = sock.recvfrom(1024)#Recieve data from server
+         processtime, address = sock.recvfrom(1024)#Recieve process time from server
+         print "\nthe output is\n" + str(data)
+         print "the Process Time\t" + str(processtime) +"\n\n"
+         number = number-1
+    sock.close()
+
+
+
+if __name__ == '__main__':
+
+
+
+          parser = argparse.ArgumentParser()#Define argument parser
+          parser.add_argument("-s","--serverIP",help="Server IP address",type=str)
+          parser.add_argument("-p","--port",help="Port Number",type=int)
+          parser.add_argument("-c","--command",help="Linux Command")
+          parser.add_argument("-d","--delay",help="Delay between two execution", type= float)
+          parser.add_argument("-n","--number",help="Command Execution number", type=int)
+          args = parser.parse_args()
+
+          if not args.serverIP or  not args.port :
+                 print("Need ServerIP and Port number")
+
+          elif args.number and (args.number < 0) :
+                 print("Please give valid execution count")
+          
+          elif  (args.number == 0):
+                 print("Please give non zero execution count")
+
+          elif args.delay and (args.delay < 0) :
+                 print("Please give valid delay time in sec.")
+
+          else :
+                client()
+
+
---
+++ UDPserver.py
@@ -0,0 +1,65 @@
+import socket
+import subprocess
+import sys
+import os
+import time
+import argparse
+import datetime
+def server():
+         # host = socket.gethostbyname(server-2.NSP-Lab03.ch-geni-net.instageni.rnoc.gatech.edu)
+         # host = '127.0.0.1'
+          host = socket.gethostname()#Get Host Name
+          print host
+          port = args.port#Get port name from the 
+
+          sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#create socket
+          sock.bind((host, port))
+
+          print "server is working"
+          while True:
+                cmd, address = sock.recvfrom(1024)#receive command from the client
+                delay, address = sock.recvfrom(1024)#receive delay from client
+                number, address = sock.recvfrom(1024)#recieve number from the client
+
+                print "connection from:\t" + str(address) #print client address
+                print "The Date and Time\t"
+                print datetime.datetime.now() #Print date and time at the time of connection
+                print "\nfrom connected client command is\n" + str(cmd) 
+                print "\nfrom connected client delay is\n" + str(delay)
+                print "\nfrom connected client execution count is\n" + str(number)
+                number = int(number)
+                delay = float(delay)
+                if cmd != str('None'):
+                   while (number):
+                        time1 = time.time()
+                        p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        out, err = p.communicate()
+                        time2 = time.time()
+                        processtime = time2 - time1
+                        processtime = str(processtime)
+                        if out:
+                                sock.sendto(out, address)#send output to client
+                                sock.sendto(processtime, address)#send process time to client
+                        else :
+                                sock.sendto(err, address)
+                        time.sleep(delay)
+                        print "\nDelay in between commands\n", delay
+                        number = number-1
+                print "server is working"
+          sock.close()
+
+
+if __name__ == '__main__':
+
+          parser = argparse.ArgumentParser()#Define the argument perser
+          parser.add_argument("-p","--port",help="Port Number",type=int)
+          args = parser.parse_args()
+          if not args.port :
+                 print("Need Port number")
+
+          else :
+                server()#Run server
+
+
+
+
